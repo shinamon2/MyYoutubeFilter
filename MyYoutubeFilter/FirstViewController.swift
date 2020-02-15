768abd202d33ed1479f689e21881f66ee5ebd38e
a.naga <shinamonphon+1@gmail.com>
2020-02-15 16:27:55 +0900

    [add]create  new project.

---
+++ MyYoutubeFilter/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MyYoutubeFilter
+//
+//  Created by aimon on 2020/02/15.
+//  Copyright © 2020 a.naga. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MyYoutubeFilter/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MyYoutubeFilter
+//
+//  Created by aimon on 2020/02/15.
+//  Copyright © 2020 a.naga. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
